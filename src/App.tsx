7b1196b073a16f8d523c228199dbca216b79fe03
Kevin Arellano <Kevin.Arellano94@GMail.Com>
2024-07-19 08:52:44 -0400

    Added environment variable

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-// src/App.tsx
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
-import About from './pages/About';
-import Contact from './pages/Contact';
 import Orders from './pages/Orders';
+
 
 const App: React.FC = () => {
   return (
@@ -15,12 +13,6 @@ const App: React.FC = () => {
             <Link to="/">Home</Link>
           </li>
           <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-          <li>
             <Link to="/orders">Orders</Link>
           </li>
         </ul>
@@ -28,8 +20,6 @@ const App: React.FC = () => {
 
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
         <Route path="/orders" element={<Orders />} />
       </Routes>
     </Router>
