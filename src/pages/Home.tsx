d5fa8b0e3ebe595715c56202620bd73d794f451f
Kevin Arellano <Kevin.Arellano94@GMail.Com>
2024-07-19 22:28:13 -0400

    Removed test environment variable

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,11 +2,9 @@ import React from 'react';
 
 
 const Home: React.FC = () => {
-  const derp = import.meta.env.VITE_DERP;
   return (
     <div>
       <h2>Home Page</h2>
-      <p>derp: { derp }</p>
     </div>
   );
 };
