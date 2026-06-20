9e27eadab5b3d5a26cf063a3781108255056f52d
Kevin Arellano <kevarellano@proton.me>
2026-06-20 19:09:13 -0400

    ugh

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,34 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Gallery from './pages/Gallery';
 
+const HideStaticContent: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const location = useLocation();
+
+  useEffect(() => {
+    const staticContent = document.querySelector('.layout') as HTMLElement;
+    if (staticContent) {
+      if (location.pathname === '/') {
+        staticContent.style.display = '';
+      } else {
+        staticContent.style.display = 'none';
+      }
+    }
+  }, [location.pathname]);
+
+  return <>{children}</>;
+};
 
 const App: React.FC = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/gallery" element={<Gallery />} />
-        <Route path="/gallery-1" element={<Gallery />} />
-      </Routes>
+      <HideStaticContent>
+        <Routes>
+          <Route path="/" element={null} />
+          <Route path="/gallery" element={<Gallery />} />
+          <Route path="/gallery-1" element={<Gallery />} />
+        </Routes>
+      </HideStaticContent>
     </Router>
   );
 };
