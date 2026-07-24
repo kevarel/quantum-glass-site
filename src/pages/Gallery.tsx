fe23ffa5c876f48ff173a2e8eed246269082cf80
Kevin Arellano <kevarellano@proton.me>
2026-07-24 19:05:40 -0400

    Added more images.

--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -36,6 +36,78 @@ const Gallery: React.FC = () => {
             src="https://quantum-glass-site.s3.amazonaws.com/images/448491759_986454036425003_6595243150635016595_n.jpg"
             className="gallery" alt="Custom glass project"
           />
+          <img
+            src="https://quantum-glass-site.s3.us-east-1.amazonaws.com/images/cd3c8565-74b5-4023-b892-efd01a25c4d6.jpg"
+            className="gallery" alt="Glass installation"
+          />
+          <img
+            src="https://quantum-glass-site.s3.us-east-1.amazonaws.com/images/552572b3-b502-48d5-97a9-62973843291b.jpg"
+            className="gallery" alt="Glass installation"
+          />
+          <img
+            src="https://quantum-glass-site.s3.us-east-1.amazonaws.com/images/a66a17cd-d48c-439c-af1d-9a9bb83c8fa8.jpg"
+            className="gallery" alt="Glass installation"
+          />
+          <img
+            src="https://quantum-glass-site.s3.us-east-1.amazonaws.com/images/b4bed6f1-6552-4910-9199-6339da6d9371.jpg"
+            className="gallery" alt="Glass installation"
+          />
+          <img
+            src="https://quantum-glass-site.s3.us-east-1.amazonaws.com/images/9a3172d1-4c97-4dee-9aee-6707383ff271.jpg"
+            className="gallery" alt="Glass installation"
+          />
+          <img
+            src="https://quantum-glass-site.s3.us-east-1.amazonaws.com/images/06452387-8bdf-4dd5-95e3-f59efad25cc1.jpg"
+            className="gallery" alt="Glass installation"
+          />
+          <img
+            src="https://quantum-glass-site.s3.us-east-1.amazonaws.com/images/a60c52df-e6d7-465b-86ae-e4cd2f3b6c6c.jpg"
+            className="gallery" alt="Glass installation"
+          />
+          <img
+            src="https://quantum-glass-site.s3.us-east-1.amazonaws.com/images/042f5cd6-1769-44e0-a13d-b1c03c6e39d0.jpg"
+            className="gallery" alt="Glass installation"
+          />
+          <img
+            src="https://quantum-glass-site.s3.us-east-1.amazonaws.com/images/f0e09858-f047-4b85-8997-9f31477a06ad.jpg"
+            className="gallery" alt="Glass installation"
+          />
+          <img
+            src="https://quantum-glass-site.s3.us-east-1.amazonaws.com/images/e482a9c1-5a1c-43d1-947a-de94ac406e9d.jpg"
+            className="gallery" alt="Glass installation"
+          />
+          <img
+            src="https://quantum-glass-site.s3.us-east-1.amazonaws.com/images/24faff93-1e1c-4f2e-9d6e-e1cc47fa64c3.jpg"
+            className="gallery" alt="Glass installation"
+          />
+          <img
+            src="https://quantum-glass-site.s3.us-east-1.amazonaws.com/images/108195ab-83cb-4615-ba6c-bd68a326789b.jpg"
+            className="gallery" alt="Glass installation"
+          />
+          <img
+            src="https://quantum-glass-site.s3.us-east-1.amazonaws.com/images/10da9575-8b06-4ace-9d2b-6b49bb987761.jpg"
+            className="gallery" alt="Glass installation"
+          />
+          <img
+            src="https://quantum-glass-site.s3.us-east-1.amazonaws.com/images/68b91392-663c-490d-b75c-b4baa41c96a1.jpg"
+            className="gallery" alt="Glass installation"
+          />
+          <img
+            src="https://quantum-glass-site.s3.us-east-1.amazonaws.com/images/3f7aa28b-b180-467a-a6d8-e7521ef9ed5c.jpg"
+            className="gallery" alt="Glass installation"
+          />
+          <img
+            src="https://quantum-glass-site.s3.us-east-1.amazonaws.com/images/159206c5-7da6-410b-85b0-80587acc1a19.jpg"
+            className="gallery" alt="Glass installation"
+          />
+          <img
+            src="https://quantum-glass-site.s3.us-east-1.amazonaws.com/images/22b53b86-8e14-45eb-9da3-2f7764877f73.jpg"
+            className="gallery" alt="Glass installation"
+          />
+          <img
+            src="https://quantum-glass-site.s3.us-east-1.amazonaws.com/images/07647648-463c-48de-ba21-b32dc2784698.jpg"
+            className="gallery" alt="Glass installation"
+          />
         </div>
       </div>
     </div>
